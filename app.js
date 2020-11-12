2fc946a5667352c9de29dae8b721c3050f80153b
Ganesh Kasar <ganekasar@gmail.com>
2020-11-12 21:27:49 +0530

    Score Evaluation added

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.post("/createtest", function(req, res) {
     const opt2 = req.body.option2;
     const opt3 = req.body.option3;
     const opt4 = req.body.option4;
+    const ans = req.body.answer;
 
     const item = new Question({
         question: ques,
@@ -54,6 +55,7 @@ app.post("/createtest", function(req, res) {
         option2: opt2,
         option3: opt3,
         option4: opt4,
+        answer: ans
     });
 
     item.save();
@@ -83,22 +85,43 @@ app.get("/test", function(req, res){
 app.post("/test", function(req, res){
     const len = req.body.length;
     var pref1 = "q";
+    var answers = [];
     //var pref2 = "qid";
 
     for(var i = 0; i < len; i++) {
         var str1 = pref1.concat(i.toString());
         var ans = req.body[str1];
+        answers.push(ans);
         //var str2 = pref2.concat(i.toString());
         //var ques_id = req.body[str2];
 
         //console.log(ques_id);
     }
 
+    Question.find({}, function(err, foundQuestions) {
+        if(err) {
+            console.log("Error occured while fetching data from database!");
+            res.redirect("/createtest");
+        } else {
+            var score = 0;
+
+            for(var i = 0; i < foundQuestions.length; i++) {
+                if(foundQuestions[i].answer === answers[i]) {
+                    score = score + 1;
+                }
+            }
+
+            console.log(score);
+
+            res.render("testsubmit", {score: score});
+        }
+    });
+
     //console.log(req.body.);
 
     //console.log(ans);
 
-    res.render("testsubmit");
+    //res.render("testsubmit");
 })
 
 app.listen(3000, function(){
