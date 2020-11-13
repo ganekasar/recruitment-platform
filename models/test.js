ca8c61a67abca8c04e63dc74f52b4846a8715695
EC2 Default User <ec2-user@ip-172-31-36-121.us-east-2.compute.internal>
2020-11-13 09:31:18 +0000

    Boiler Plate is created. Multiple test functionalities and Changes made by sudhanshu are absent

---
+++ models/test.js
@@ -0,0 +1,9 @@
+var mongoose              = require("mongoose");
+const Schema = mongoose.Schema;
+var test;
+
+var testSchema = new Schema({
+    name: String
+});
+
+module.exports = test = mongoose.model("Test" , testSchema);
--- sample.js
+++ sample.js
@@ -1,55 +1,81 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+var express         = require("express"),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    localStrategy   = require("passport-local"),
+    bodyParser      = require("body-parser"),
+    Candidate       = require("./models/candidate"),
+    Test            = require("./models/test"),
+    Question        = require("./models/question"),
+    Response        = require("./models/response");
 
 var app = express();
-
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname+"/public"));
 
-mongoose.connect('mongodb://localhost:27017/recruitdb', {useNewUrlParser: true,useUnifiedTopology: true});
 
-const testSchema = {
-    name: String
-};
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static("public"));
 
-const questionsSchema = {
-    test: testSchema,
-    question: String,
-    option1: String,
-    option2: String,
-    option3: String,
-    option4: String,
-    answer: String
-};
 
-const responseSchema = {
-    answer: String,
-    question: questionsSchema
-};
+mongoose.connect("mongodb://localhost/project");
 
-const Test = mongoose.model("Test", testSchema);
 
-const Question = mongoose.model("Question", questionsSchema);
 
-const Response = mongoose.model("Response", responseSchema);
-
-app.get("/", function(req, res) {
-    res.render("landing");
-});
+app.get("/", function(req, res){
+    res.render("landing.ejs");
+})
 
 app.get("/company", function(req, res){
-    res.render("companyLanding");
+    res.render("companyLanding.ejs");
+})
+
+
+//STUDENT ROUTES
+
+app.get("/studentRegister", function(req, res){
+    res.render("studentRegister.ejs");
 });
+
+
+app.get("/candidates", function(req, res){
+    res.send("HI");
+})
+
+//CREATE ROUTE
+app.post("/candidates" , function(req , res){
+   var newName = req.body.candidate_name;
+   var newInstitute = req.body.institute;
+   var newEmail = req.body.email;
+   var newLinkedin = req.body.linkedin;
+   var candidateObject = {name : newName , Institute:newInstitute, email : newEmail, LinkedIn :newLinkedin};
+   //Create new candidate and add to the database
+   Candidate.create(candidateObject , function(err , newCandidate){
+       if(err){
+           console.log(err);
+       } else{
+           // redirect 
+           res.redirect("/")
+       }
+   })
+});
+
+
+
+
+app.get("/studentLogin", function(req, res){
+    res.render("studentLogin.ejs");
+});
+
+
+//TEST ROUTES
+
 
 app.get("/createtest", function(req, res){
     res.render("createTest");
 });
 
 app.post("/createtest", function(req, res) {
-    const name = req.body.name;
+    var name = req.body.name;
 
     const item = new Test({
         name: name
@@ -65,11 +91,7 @@ app.get("/selecttest", function(req, res) {
         if(err) {
             res.redirect("/createtest");
         } else {
-          if(foundTests ==0){
-            res.render("notest");
-          }else{
-          res.render("selecttest", {foundTests : foundTests});
-          }
+            res.render("selecttest", {foundTests : foundTests});
         }
     });
 });
@@ -89,13 +111,13 @@ app.get("/:id/managetest", function(req, res) {
     console.log(test);
 
     res.render("managetest", {test: test});
-
+    
     // Test.find({_id: test}, function(err, foundTest) {
     //     if(err) {
     //         res.redirect("/createtest");
     //     } else {
 
-
+            
     //         // Question.find({}, function(err, foundQuestions) {
     //         //     if(err) {
     //         //         console.log("Error occured while fetching data from database!");
@@ -107,7 +129,7 @@ app.get("/:id/managetest", function(req, res) {
     //     }
     // });
 
-
+    
 
     //res.render("managetest");
 });
@@ -119,9 +141,7 @@ app.post("/managetest", function(req, res) {
     const opt3 = req.body.option3;
     const opt4 = req.body.option4;
     const ans = req.body.answer;
-    const test = new Test({
-          name: req.body.test
-    });
+    const test = req.body.test;
 
     const item = new Question({
         question: ques,
@@ -134,8 +154,8 @@ app.post("/managetest", function(req, res) {
     });
 
     item.save();
-    res.render("addedsuccessfully",{testname:test.name})
-    //res.redirect("/"+test.name+"/managetest");
+
+    res.redirect("/" + test + "/test");
 })
 
 app.get("/signup", function(req, res){
@@ -147,18 +167,14 @@ app.get("/login", function(req, res){
 });
 
 app.get("/:id/test", function(req, res){
+    
+    const test = req.params.id;
 
-    const test = new Test({
-      name:req.params.id
-    });
-//  console.log(test);
-
-    Question.find({test : {name:test.name}}, function(err, foundQuestions) {
+    Question.find({test : test}, function(err, foundQuestions) {
         if(err) {
             console.log("Error occured while fetching data from database!");
             res.redirect("/createtest");
         } else {
-  //        console.log("Sudhanshu");
             res.render("test", {foundQuestions: foundQuestions});
         }
     });
@@ -194,7 +210,7 @@ app.post("/test", function(req, res){
             }
         })
 
-
+        
         //var str2 = pref2.concat(i.toString());
         //var ques_id = req.body[str2];
 
@@ -225,40 +241,11 @@ app.post("/test", function(req, res){
     //console.log(ans);
 
     //res.render("testsubmit");
-});
+})
 
-app.get("/viewtest",function(req,res){
-  Test.find({}, function(err, foundTests) {
-      if(err) {
-          res.redirect("/createtest");
-      } else {
-        if(foundTests ==0){
-          res.render("notest");
-        }else{
-        res.render("selectviewtest", {foundTests : foundTests});
-        }
-      }
-  });
-});
 
-app.get("/:id/viewtest", function(req, res){
 
-    const test = new Test({
-      name:req.params.id
-    });
-//  console.log(test);
 
-    Question.find({test : {name:test.name}}, function(err, foundQuestions) {
-        if(err) {
-            console.log("Error occured while fetching data from database!");
-            res.redirect("/createtest");
-        } else {
-  //        console.log("Sudhanshu");
-            res.render("test", {foundQuestions: foundQuestions});
-        }
-    });
-});
-
-app.listen(3000, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER HAS STARTED!");
 });
